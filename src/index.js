7ee952d76b2d8e4b5aec0ddf92b124688cb4e73e
Leonard Lamprecht <mindrun@icloud.com>
2016-04-30 21:47:02 +0200

    Respect older options

--- src/index.js
+++ src/index.js
@@ -4,65 +4,41 @@ import path from 'path'
 
 class Commander {
   constructor() {
-    this.args = parser(process.argv.slice(1))
-    const parent = module.parent
-
-    pkginfo(parent)
-    const version = parent.exports.version
-
     this.details = {
       options: [],
       commands: []
     }
 
-    if (version) {
-      this.option('version', 'Output the version number', version)
-
-      if (this.args.v || this.args.version) {
-        console.log(version)
-        process.exit()
-      }
-    }
-
     this.option('help', 'Output usage information')
     this.command('help', 'Display help')
-
-    if (this.args._[1] == 'help' || this.args.h || this.args.help) {
-      this.renderHelp()
-    }
-
-    const args = this.args
-    args._.shift()
-
-    this.raw = args
   }
 
   option (name, description, defaultValue) {
-    let variants = []
+    let usage = []
 
     switch (name.constructor) {
       case String:
-        variants[0] = name.charAt(0)
-        variants[1] = name
+        usage[0] = name.charAt(0)
+        usage[1] = name
 
         break
       case Array:
-        variants = variants.concat(name)
+        usage = usage.concat(name)
         break
       default:
         throw new Error('Invalid name for option')
     }
 
-    if (variants.length > 0 && variants[0].length > 1) {
+    if (usage.length > 0 && usage[0].length > 1) {
       throw new Error('Short version of option is longer than 1 char')
     }
 
     this.details.options.push({
-      usage: `-${variants[0]}, --${variants[1]}`,
+      initial: defaultValue || false,
+      usage,
       description
     })
 
-    this.setProperties(variants, defaultValue)
     return this
   }
 
@@ -92,6 +68,16 @@ class Commander {
   generateDetails (kind) {
     const items = this.details[kind]
     let parts = []
+
+    for (let item in items) {
+      let usage = items[item].usage
+
+      if (usage.constructor === Array) {
+        usage = `-${usage[0]}, --${usage[1]}`
+      }
+
+      items[item].usage = usage
+    }
 
     const longest = items.sort((a, b) => {
       return b.usage.length - a.usage.length
@@ -136,6 +122,37 @@ class Commander {
 
     console.log(details.join('\n  '))
     process.exit()
+  }
+
+  parse (argv) {
+    this.args = parser(argv.slice(1))
+
+    const parent = module.parent
+
+    pkginfo(parent)
+    const version = parent.exports.version
+
+    if (version) {
+      this.option('version', 'Output the version number', version)
+
+      if (this.args.v || this.args.version) {
+        console.log(version)
+        process.exit()
+      }
+    }
+
+    if (this.args._[1] == 'help' || this.args.h || this.args.help) {
+      this.renderHelp()
+    }
+
+    for (let option of this.details.options) {
+      this.setProperties(option.usage, option.initial)
+    }
+
+    const args = this.args
+    args._.shift()
+
+    this.raw = args
   }
 }
 
