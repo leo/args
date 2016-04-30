9c912fbaa9f8805c5861c03302d2ab27ba6599c3
Leonard Lamprecht <mindrun@icloud.com>
2016-04-30 16:50:01 +0200

    Set instance properties

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
-class Parser {
+import parser from 'minimist'
+
+class Commander {
   constructor() {
-    this.argv = process.argv
+    this.args = parser(process.argv.slice(2))
   }
 
   option (name, description, defaultValue) {
-    let variants = {}
+    let variants = []
 
     switch (name.constructor) {
       case String:
-        variants.short = name.charAt(0)
-        variants.long = name
+        variants[0] = name.charAt(0)
+        variants[1] = name
+
         break
       case Array:
-        variants.short = name[0]
-        variants.long = name[1]
+        variants.concat(name)
         break
       default:
         console.error(`Invalid name for option ${name}`)
     }
 
-    for (let kind in variants) {
-      const value = variants[kind]
-      this[value] = this.getArgument(value) || defaultValue
-    }
-
+    this.setProperties(variants, defaultValue)
     return this
   }
 
-  setInstanceProperty (name, initialValue) {
-    this[name] = this.getArgument(name) || defaultValue
-  }
+  setProperties (names, initial) {
+    let value = false
 
-  getArgument (name, kind) {
-    return false
+    for (let name of names) {
+      let fromArgs = this.args[name]
+
+      if (fromArgs) {
+        value = fromArgs
+      }
+    }
+
+    for (let name of names) {
+      this[name] = value
+    }
   }
 }
 
-export default new Parser
+export default new Commander
