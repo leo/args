f84e71aa6da2064ad1a176d97a2d0bdfb4066da1
ntwcklng <ntwcklng@gmail.com>
2017-07-27 16:38:42 +0200

    -nanomist +mri

--- test/index.js
+++ test/index.js
@@ -125,10 +125,10 @@ test('command aliases', async t => {
   }
 });
 
-test('options propogated to nanomist', t => {
+test('options propogated to mri', t => {
   args.option('port', 'The port on which the site will run');
 
-  const config = args.parse(argv, { nanomist: { string: 'p' } });
+  const config = args.parse(argv, { mri: { string: 'p' } });
 
   t.is(config.port, port.toString());
 });
