a8ce20490fb4d465d3c16b15b507d4e7c95a58ef
ntwcklng <ntwcklng@gmail.com>
2018-04-09 17:41:02 +0200

    run prettier

--- test/_fixture.js
+++ test/_fixture.js
@@ -1,27 +1,27 @@
-const args = require('../lib');
+const args = require('../lib')
 
 args.command(
   'install',
   'desc here',
   () => {
-    console.log('install');
+    console.log('install')
   },
   ['i']
-);
+)
 
 args.command(
   'uninstall',
   'another desc here',
   () => {
-    console.log('uninstall');
+    console.log('uninstall')
   },
   ['u', 'rm', 'remove']
-);
+)
 
 args.command('cmd', 'cmd desc', () => {
-  console.log('^~^');
-});
+  console.log('^~^')
+})
 
-args.command('binary', 'some desc', ['b']);
-args.option(['a', 'abc'], 'something', 'def value');
-args.parse(process.argv);
+args.command('binary', 'some desc', ['b'])
+args.option(['a', 'abc'], 'something', 'def value')
+args.parse(process.argv)
